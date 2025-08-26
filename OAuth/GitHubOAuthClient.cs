585b28bdc855a7d47b8c059805a9abb863bf666d
zabronax <zabronax@gmail.com>
2025-08-26 10:04:24 +0200

    add oauth app info

--- OAuth/GitHubOAuthClient.cs
+++ OAuth/GitHubOAuthClient.cs
@@ -1,6 +1,9 @@
 
 class GitHubOAuthClient : IOAuthClient
 {
+  private string clientId = "Ov23liuZkpx9rTE4jQ89";
+  private string clientSecret = "1e46f6893616983f5e213ec1bd66a842981a798e";
+
   public Task<string> GetProfileInfo()
   {
     throw new NotImplementedException();
