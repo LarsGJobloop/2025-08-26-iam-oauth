47187505fa205ebb32dbbf2502749ea8e7104b83
zabronax <zabronax@gmail.com>
2025-08-26 09:43:48 +0200

    fix missing semicolons

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Login use via GitHub
-Console.WriteLine("Loging in via GitHub OAuth")
+Console.WriteLine("Loging in via GitHub OAuth");
 
 // Console log User Info
-Console.WriteLine("Printing GitHub profile info")
+Console.WriteLine("Printing GitHub profile info");
