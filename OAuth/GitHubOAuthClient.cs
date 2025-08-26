265b2389d6146a495e3499b9a509ab19c34d98fb
zabronax <zabronax@gmail.com>
2025-08-26 11:52:51 +0200

    comment out token logging

--- OAuth/GitHubOAuthClient.cs
+++ OAuth/GitHubOAuthClient.cs
@@ -78,7 +78,7 @@ class GitHubOAuthClient : IOAuthClient
     var tokenResponse = await http.SendAsync(tokenRequest);
     string tokenJson = await tokenResponse.Content.ReadAsStringAsync();
     // WARNING! Never expose the token it's a password!
-    Console.WriteLine($"Returned token: {tokenJson}");
+    // Console.WriteLine($"Returned token: {tokenJson}");
     // We assume that the GitHub API is stable.
     string accessToken = JsonDocument.Parse(tokenJson).RootElement.GetProperty("access_token").GetString()!;
 
