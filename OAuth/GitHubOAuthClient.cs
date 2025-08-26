2631335893c06553458a7f0f171239a38da87a06
zabronax <zabronax@gmail.com>
2025-08-26 11:04:01 +0200

    fix missing trailing slash in callback

--- OAuth/GitHubOAuthClient.cs
+++ OAuth/GitHubOAuthClient.cs
@@ -8,7 +8,7 @@ class GitHubOAuthClient : IOAuthClient
   private static readonly string clientId = "Ov23liuZkpx9rTE4jQ89";
   private static readonly string clientSecret = "1e46f6893616983f5e213ec1bd66a842981a798e";
   // This is you configuring your registered OAuth app with the IdP
-  private static readonly string redirectUri = "http://127.0.0.1:20000/callback";
+  private static readonly string redirectUri = "http://127.0.0.1:20000/callback/";
 
   // These are rather static IdP, and specific to the IdP
   private static readonly string authorizeUrl = "https://github.com/login/oauth/authorize";
